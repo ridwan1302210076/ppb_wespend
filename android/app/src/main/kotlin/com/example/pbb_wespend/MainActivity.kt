78ecc9ff6670485b64852b7193860786e7c58c8c
bagheera30 <rizkialfian30103@gmail.com>
2024-03-13 00:13:45 +0700

    intruduction:login & home

---
+++ android/app/src/main/kotlin/com/example/pbb_wespend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pbb_wespend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:pbb_wespend/home.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isEmailValid = true;
+  bool isPasswordVisible = false;
+
+  void validateEmail(String email) {
+    setState(() {
+      if (email.isEmpty) {
+        isEmailValid = true;
+      } else {
+        isEmailValid = email.endsWith("@gmail.com");
+      }
+    });
+  }
+
+  void togglePasswordVisibility() {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+
+  void submitForm() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const Home(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Masuk",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Email",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Masukkan Email',
+                          ),
+                          onChanged: (value) {
+                            validateEmail(value);
+                          },
+                        ),
+                        if (!isEmailValid)
+                          Text(
+                            "Email harus berakhir dengan @gmail.com",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Password",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                            hintText: 'Masukkan Password',
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                isPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: togglePasswordVisibility,
+                            ),
+                          ),
+                          obscureText: !isPasswordVisible,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: submitForm,
+                child: const Text("Submit"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // Warna latar belakang
+                  onPrimary: Colors.white, // Warna teks
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
